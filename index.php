07b9004521cc0cfd1255184d80b4f4ae458d080d
Hendrik Bahr <bahr@fatchip.de>
2022-02-05 13:41:01 +0100

    Use default colors, add exercises, remove generated csv

--- index.php
+++ index.php
@@ -22,35 +22,7 @@
             background-color: rgb(204, 82, 0);
             border-color: rgb(179, 71, 0);
         }
-        .btn-outline-primary:active {
-            color: #fff;
-            background-color: #ff6600;
-            border-color: #ff6600;
-        }
-        .btn-primary {
-            background-color:#ff6600;
-            border-color: #ff6600;        }
-        .btn-primary:hover {
-            background-color:#ff6600;
-            border-color:#ff6600;
-        }
-        .btn-primary:focus {
-            background-color: #ff6600;
-            border-color: #ff6600;
-            box-shadow: 0 0 0 0.25rem #ff660050;
-        }
-        .btn-primary:active:focus {
-            box-shadow: 0 0 0 0.25rem #ff660050;
-        }
-       
     </style>
-<!--<script-->
-<!--        src="https://code.jquery.com/jquery-3.6.0.js"-->
-<!--        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="-->
-<!--        crossorigin="anonymous">-->
-<!--</script>-->
-
-
 </head>
 <body>
     <div class="d-flex p-2 flex-column justify-content-center align-items-center ">
@@ -72,8 +44,6 @@
         <button id="copy-to-clipboard-button" type="button" class="btn btn-primary btn-lg" style="width: 100%" > Copy to clipboard</button>
         <textarea  style="opacity: .01;height:0;position:absolute;z-index: -1; " id="toClipboard"></textarea>
     </div>
-
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
