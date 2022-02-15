08b3ba3d9d9ba7896e51a18e84ff265766876cda
Mikolai Hecht <mikolai.hecht@fatchip.de>
2022-02-15 12:10:26 +0100

    Angefuangen

--- index.php
+++ index.php
@@ -1,39 +1,5 @@
 <html>
-<head>
-    <title>Workout Generator</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="js/jquery.js"></script>
-    <script src="js/jquery-csv.js"></script>
-    <style>
-        .btn-check:active+.btn-outline-primary,
-        .btn-check:checked+.btn-outline-primary,
-        .btn-outline-primary.active,
-        .btn-outline-primary.dropdown-toggle.show,
-
-        .dark-mode{
-            color: rgb(209, 205, 199);
-            background-color: rgb(24, 26, 27);
-            -webkit-tap-highlight-color: transparent;
-        }
-        .btn-darkmode{
-            background-color: rgb(204, 82, 0);
-            border-color: rgb(179, 71, 0);
-        }
-    </style>
-</head>
+<?php include "header.php"; ?>
 <body>
     <div class="d-flex p-2 flex-column justify-content-center align-items-center ">
         <h1>Workout Generator</h1>
@@ -59,8 +25,6 @@
     <div class="d-flex p-2 flex-column justify-content-center align-items-center ">
         <a class="btn btn-primary btn-lg" style="width: 100%" href="edit.php" > EDIT</a>
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 <script type="text/javascript">
 
@@ -80,12 +44,12 @@
     }
 
     function loadWorkout() {
-        var DATEWorkout = "";
-        $.get("Controller/ajax.php?action=getFileAge",function (data) {
-            $("#fileAge").html("Das Workout wurde am "+data+" erstellt");
-            DATEWorkout = data;
-            console.log(data);
-        });
+        var DATEWorkout = "<?= (isset($_SESSION["workout"]))? $_SESSION["workout"]["date"]:'' ?>";
+        // $.get("Controller/ajax.php?action=getFileAge",function (data) {
+        //     $("#fileAge").html("Das Workout wurde am "+data+" erstellt");
+        //     DATEWorkout = data;
+        //     console.log(data);
+        // });
 
             $.getJSON("Controller/ajax.php?action=getWorkout",function (data) {
             console.log(data);
