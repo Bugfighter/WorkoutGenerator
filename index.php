63c8968a9b7c2b8ce0b23ad4d4e5b811afcc2432
Hendrik Bahr <bahr@fatchip.de>
2025-04-09 21:54:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -25,89 +25,89 @@
     <div class="d-flex p-2 flex-column justify-content-center align-items-center ">
         <a class="btn btn-secondary btn-lg" style="width: 100%" href="edit.php" > EDIT</a>
     </div>
-</body>
-<script type="text/javascript">
-
-    var WorkoutCsv ;
-    var uebungenCSV ;
-    $(document).ready(function(){
-        loadWorkout();
-        $("#generate").click(function(){
-            generateNew();
-        });
-
-    });
-    function generateNew() {
-        $.get("Controller/ajax.php?action=writeNewWorkout",function () {
+    <script type="text/javascript">
+    
+        var WorkoutCsv ;
+        var uebungenCSV ;
+        $(document).ready(function(){
             loadWorkout();
-        });
-    }
-
-    function loadWorkout() {
-        var DATEWorkout = "";
-        $.get("Controller/ajax.php?action=getFileAge",function (data) {
-            if(data !== ""){
-                $("#fileAge").html("Das Workout wurde am "+data+" erstellt");
-                DATEWorkout = data;
-                console.log(data);
-            }
-        });
-
-            $.getJSON("Controller/ajax.php?action=getWorkout",function (data) {
-            console.log(data);
-            var div = $("#workoutplan");
-            div.html("<table>");
-            var descCounter = 0;
-            var oldhtml ="";
-            var skipFirst = true;
-            $.each(data, function( index, value ) {
-                if(skipFirst){
-                    skipFirst=false;
-                    return;
-                }
-                descCounter++;
-                oldhtml = div.html();
-                oldhtml = oldhtml + "<tr>"
-                    + "<td>" + value[0]+": </td> "
-                    + "<td><button data-toggle='collapse' data-target='#descCollapse"+ descCounter +"' class='btn btn-secondary'>" + value[1]+"</button></td>"
-                    + "<tr class='collapse' id='descCollapse"+ descCounter +"'> <td colspan='2'> <p>"+value[2]+"</P> </td></tr>"
-                    + "</tr>";
-                div.html(oldhtml);
+            $("#generate").click(function(){
+                generateNew();
             });
-                var clipboard = $("#toClipboard");
-                var clipboardText ="";
-                skipFirst = true;
+    
+        });
+        function generateNew() {
+            $.get("Controller/ajax.php?action=writeNewWorkout",function () {
+                loadWorkout();
+            });
+        }
+    
+        function loadWorkout() {
+            var DATEWorkout = "";
+            $.get("Controller/ajax.php?action=getFileAge",function (data) {
+                if(data !== ""){
+                    $("#fileAge").html("Das Workout wurde am "+data+" erstellt");
+                    DATEWorkout = data;
+                    console.log(data);
+                }
+            });
+    
+                $.getJSON("Controller/ajax.php?action=getWorkout",function (data) {
+                console.log(data);
+                var div = $("#workoutplan");
+                div.html("<table>");
+                var descCounter = 0;
+                var oldhtml ="";
+                var skipFirst = true;
                 $.each(data, function( index, value ) {
                     if(skipFirst){
                         skipFirst=false;
-                        clipboardText = clipboardText +DATEWorkout+"\n";
                         return;
                     }
-                       clipboardText = clipboardText+"*"+value[1].trim()+"*" +"("+value[2]+")\n";
-
+                    descCounter++;
+                    oldhtml = div.html();
+                    oldhtml = oldhtml + "<tr>"
+                        + "<td>" + value[0]+": </td> "
+                        + "<td><button data-toggle='collapse' data-target='#descCollapse"+ descCounter +"' class='btn btn-secondary'>" + value[1]+"</button></td>"
+                        + "<tr class='collapse' id='descCollapse"+ descCounter +"'> <td colspan='2'> <p>"+value[2]+"</P> </td></tr>"
+                        + "</tr>";
+                    div.html(oldhtml);
+                });
+                    var clipboard = $("#toClipboard");
+                    var clipboardText ="";
+                    skipFirst = true;
+                    $.each(data, function( index, value ) {
+                        if(skipFirst){
+                            skipFirst=false;
+                            clipboardText = clipboardText +DATEWorkout+"\n";
+                            return;
+                        }
+                           clipboardText = clipboardText+"*"+value[1].trim()+"*"+" ("+value[2]+")\n";
+    
+                });
+                    clipboard.html(clipboardText);
+                });
+        }
+    
+        function darkmode() {
+            var element = document.body;
+            element.classList.toggle("dark-mode");
+    
+            var element = $(".btn-primary");
+            element.classList.toggle("dark-mode");
+        }
+    
+        jQuery(document).ready(function($) {
+            $('#copy-to-clipboard-button').on('click', function(e) {
+                e.preventDefault();
+    
+                $('#toClipboard').select();
+                document.execCommand('copy');
             });
-                clipboard.html(clipboardText);
-            });
-    }
-
-    function darkmode() {
-        var element = document.body;
-        element.classList.toggle("dark-mode");
-
-        var element = $(".btn-primary");
-        element.classList.toggle("dark-mode");
-    }
-
-    jQuery(document).ready(function($) {
-        $('#copy-to-clipboard-button').on('click', function(e) {
-            e.preventDefault();
-
-            $('#toClipboard').select();
-            document.execCommand('copy');
         });
-    });
-
-</script>
+    
+    </script>
+    </body>
 </html>
 
 
